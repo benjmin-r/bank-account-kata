04323f36080d164cb60ca797175cb604532e0717
Benjamin Reitzammer <benjamin@ambestengestern.de>
2012-05-31 14:59:52 +0200

    fixing tabs

--- java/src/main/java/bank/Money.java
+++ java/src/main/java/bank/Money.java
@@ -1,19 +1,29 @@
 package bank;
 
+import java.text.MessageFormat;
+
 public class Money {
 
-	private int amount;
+    private int amount;
 
-	public Money(int amount) {
-		this.amount = amount;
-	}
+    public Money(int amount) {
+        this.amount = amount;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		Money other = (Money) obj;
-		if ( amount != other.amount )
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        Money other = (Money) obj;
+        if ( amount != other.amount )
+            return false;
+        return true;
+    }
+
+    public Money minus(Money minusAmount) {
+        return new Money(this.amount - minusAmount.amount);
+    }
+
+    public String toString() {
+        return MessageFormat.format("Money={0}", this.amount);
+    }
 
 }
--- java/src/test/java/bank/MoneyTest.java
+++ java/src/test/java/bank/MoneyTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 
 public class MoneyTest {
 
-	@Test
-	public void should_be_equal_with_same_amount() {
-		Money m1 = new Money( 101 );
-		Money m2 = new Money( 101 );
+    @Test
+    public void should_be_equal_with_same_amount() {
+        Money m1 = new Money(101);
+        Money m2 = new Money(101);
 
-		assertTrue( m1.equals( m2 ) );
-	}
+        assertTrue(m1.equals(m2));
+    }
 
-	@Test
-	public void should_be_unequal_with_different_amount() {
-		Money m1 = new Money( 101 );
-		Money m2 = new Money( 102 );
+    @Test
+    public void should_be_unequal_with_different_amount() {
+        Money m1 = new Money(101);
+        Money m2 = new Money(102);
 
-		assertFalse( m1.equals( m2 ) );
-	}
+        assertFalse(m1.equals(m2));
+    }
 }
